83f9ded7a8c2d0d1e8716d11cb229c3f164554d6
rybalidoy <rybalidoy@gmail.com>
2025-09-07 03:28:29 +0800

    added lsp and autocomplete
    - working only in php, lua, not yet functional on vue files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,53 +1,51 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
+  {
     "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "j-hui/fidget.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+            },
+          },
+        },
+        volar = {
+          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+        },
+        ts_ls = {},
+        eslint = {},
+        tailwindcss = {},
+        intelephense = {},
+      },
+    },
+    config = function(_, opts)
+      require("mason").setup()
+
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lua_ls", "eslint", "ts_ls"},
+      })
+      
+      local lspconfig = require("lspconfig")
+      
+      -- Capabilities for autocomplete (nvim-cmp / blink.cmp)
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+
+      if ok then
+        capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+      end
+
+      for server, server_opts in pairs(opts.servers) do
+        lspconfig[server].setup(server_opts)
+      end
+    end,
   },
-
-  config = function()
-    local cmp = require('cmp')
-    local cmp_lsp = require("cmp_nvim_lsp")
-    local capabilities = vim.tbl_deep_extend(
-      "force",
-      {},
-      vim.lsp.protocol.make_client_capabilities(),
-      cmp_lsp.default_capabilities()
-    )
-
-    require("fidget").setup({})
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "rust_analyzer",
-        "ts_ls",
-        "vue_ls",
-        "intelephense",
-      }
-    })
-
-    local lspconfig = require("lspconfig")
-
-    -- Lua (Neovim config)
-    lspconfig.lua_ls.setup({ capabilities = capabilities })
-
-    -- TypeScript
-    lspconfig.ts_ls.setup({ capabilities = capabilities }) 
-
-    -- Vue / Nuxt
-    lspconfig.volar.setup({
-      capabilities = capabilities,
-      filetypes = {"typescript", "javascript", "vue", "json" },
-    })
-
-  end
 }
+
--- lua/ryaai/keymaps.lua
+++ lua/ryaai/keymaps.lua
@@ -3,3 +3,35 @@ vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+vim.api.nvim_create_autocmd(
+  "LspAttach",
+  { 
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+      vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc" -- Enable completion triggered by <c-x><c-o>
+
+      -- Buffer local mappings
+      -- See `:help vim.lsp.*` for documentation on any of the below functionse
+      local opts = { buffer = ev.buf } 
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "<leader><space>", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "gi", vim.lsp.buf.type_definition, opts)
+      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+
+      vim.keymap.set("n", "<leader>f", function()
+        vim.lsp.buf.format({ async = true })
+      end, opts)
+
+      -- Open the diagnostic under the cursor in a float window
+      vim.keymap.set("n", "<leader>d", function()
+        vim.diagnostic.open_float({
+          border = "rounded",
+        })
+      end, opts)
+    end,
+  }
+)
