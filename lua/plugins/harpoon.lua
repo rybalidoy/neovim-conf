1a86e6d9b27c475bdebff42d874331bc58463a6e
rybalidoy <rybalidoy@gmail.com>
2025-09-18 19:04:37 +0800

    harpoon and lsp addition

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,9 +13,9 @@ return {
 
     -- Navigation
     vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
   end,
 }
 
